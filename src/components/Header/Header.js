d66a7b839f3ae417013371b371a342d233be1249
ZManak <javifuer@gmail.com>
2023-02-23 14:57:08 +0100

    add/MaterialaAppBar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,171 @@
-import React, { Component } from "react";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { Menu } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import AdbIcon from "@mui/icons-material/Adb";
 
-class Header extends Component {
-  render() {
-    return <div>Header</div>;
+const pages = ["Home", "News", "Create"];
+const settings = ["Login"];
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      anchorElNav: null,
+      anchorElUser: null
+    }
   }
-}
+
+  handleOpenNavMenu = (event) => {
+    this.setState({ anchorElNav: event.target });
+  };
+
+  handleOpenUserMenu = (event) => {
+    this.setState({ anchorElUser: event.target });;
+  };
+
+  handleCloseNavMenu = () => {
+    this.setState({ anchorElNav: null });
+  };
+
+  handleCloseUserMenu = () => {
+    this.setState({anchorElUser: null});
+  };
+
+  render() {
+    return (<AppBar sx={{
+      background: '#000000'
+    }} position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <AdbIcon sx={{ display: { background: '#000000', xs: "none", md: "flex" }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { background: '#000000', xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none"
+            }}
+          >
+            "PRESSROOM.net"
+          </Typography>
+
+          <Box sx={{ background: '#000000', flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={this.handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={this.state.anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left"
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left"
+              }}
+              open={Boolean(this.state.anchorElNav)}
+              onClose={this.handleCloseNavMenu}
+              sx={{
+                display: { background: '#272A31', xs: "block", md: "none" }
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={this.handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <AdbIcon sx={{ display: { background: '#000000', xs: "none", md: "none" }, mr: 1 }} />
+          <Typography
+            variant="h7"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { background: '#000000', xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 600,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none"
+            }}
+          >
+            PRESSROOM
+          </Typography>
+          <Box sx={{ background: '#000000', flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={this.handleCloseNavMenu}
+                sx={{ my: 2, color: "white", display: "block" }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+
+          <Box sx={{ background: '#000000', flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={this.handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: "45px" }}
+              id="menu-appbar"
+              anchorEl={this.state.anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right"
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right"
+              }}
+              open={Boolean(this.state.anchorElUser)}
+              onClose={this.handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={this.handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  )}
+};
 
 export default Header;
